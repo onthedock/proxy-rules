4e0c5db8b0d6e7c4dcd2276bbf3361238c785d58
Xavi Aznar <xavi.aznar@outlook.com>
2023-02-25 18:40:45 +0000

    Use rules package
    Remove function from main.go (and package main)

--- main.go
+++ main.go
@@ -3,12 +3,12 @@ package main
 import (
 	"encoding/csv"
 	"encoding/json"
-	"errors"
 	"flag"
 	"fmt"
 	"io"
 	"log"
 	"os"
+	"rules/rules"
 	"strings"
 )
 
@@ -26,7 +26,8 @@ func main() {
 	defer file.Close()
 
 	r := csv.NewReader(file)
-	rules := []*Rule{}
+	proxyRules := make([]*rules.Rule, 0)
+
 	for {
 		line, err := r.Read()
 		if err == io.EOF {
@@ -40,19 +41,19 @@ func main() {
 			line[i] = strings.TrimSpace(line[i])
 		}
 
-		// Validate fields
-		fieldErrors := validateFields(line)
-		if len(fieldErrors) > 0 {
-			for i := range fieldErrors {
-				log.Printf("error processing line %s: %s.", line, fieldErrors[i])
-			}
+		rule := new(rules.Rule)
+		var ruleErr error
+		rule, ruleErr = rules.NewRule(line)
+		fmt.Printf("%v\n", *rule)
+		if ruleErr != nil {
+			log.Printf("%v, %v", line, ruleErr)
 			continue
 		}
+		proxyRules = append(proxyRules, rule)
 
-		rules = append(rules, NewRule(line))
 	}
 
-	jsonOutput, err := json.Marshal(rules)
+	jsonOutput, err := json.Marshal(proxyRules)
 	if err != nil {
 		log.Printf("unable to convert to JSON: %s\n", err.Error())
 		os.Exit(1)
@@ -60,56 +61,45 @@ func main() {
 	os.WriteFile(*jsonFilename, jsonOutput, 0644)
 }
 
-type Rule struct {
-	Protocol string `json:"protocol"`
-	Fqdn     string `json:"fqdn"`
-	Port     string `json:"port"`
-	Action   string `json:"action"`
-}
+// type Rule struct {
+// 	Protocol string `json:"protocol"`
+// 	Fqdn     string `json:"fqdn"`
+// 	Port     string `json:"port"`
+// 	Action   string `json:"action"`
+// }
 
-func (r *Rule) String() string {
-	return fmt.Sprintf("%s %s %s %s", r.Protocol, r.Fqdn, r.Port, r.Action)
-}
+// func (r *Rule) String() string {
+// 	return fmt.Sprintf("%s %s %s %s", r.Protocol, r.Fqdn, r.Port, r.Action)
+// }
 
-func NewRule(fields []string) *Rule {
-	rule := Rule{}
+// func NewRule(fields []string) *Rule {
+// 	rule := Rule{}
 
-	for range fields {
-		rule.Protocol = fields[0]
-		rule.Fqdn = fields[1]
-		rule.Port = fields[2]
-		rule.Action = fields[3]
-	}
-	return &rule
-}
+// 	for range fields {
+// 		rule.Protocol = fields[0]
+// 		rule.Fqdn = fields[1]
+// 		rule.Port = fields[2]
+// 		rule.Action = fields[3]
+// 	}
+// 	return &rule
+// }
 
-func validateFields(fields []string) []error {
-	var validationErrors = make([]error, 0)
-	if !(fields[0] == "tcp" || fields[0] == "udp") {
-		validationErrors = append(validationErrors, fmt.Errorf("%w. %q must be one of 'tcp' or 'udp'", ErrInvalidProtocol, fields[0]))
-	}
+// func validateFields(fields []string) []error {
+// 	var validationErrors = make([]error, 0)
+// 	if !(fields[0] == "tcp" || fields[0] == "udp") {
+// 		validationErrors = append(validationErrors, fmt.Errorf("%w. %q must be one of 'tcp' or 'udp'", rules.ErrInvalidProtocol, fields[0]))
+// 	}
 
-	rangeStart, rangeEnd, found := strings.Cut(fields[2], "-")
-	if found {
-		validationErrors = append(validationErrors, validatePort(rangeStart))
-		validationErrors = append(validationErrors, validatePort(rangeEnd))
-	} else {
-		validationErrors = append(validationErrors, validatePort(fields[2]))
-	}
+// 	rangeStart, rangeEnd, found := strings.Cut(fields[2], "-")
+// 	if found {
+// 		validationErrors = append(validationErrors, validatePort(rangeStart))
+// 		validationErrors = append(validationErrors, validatePort(rangeEnd))
+// 	} else {
+// 		validationErrors = append(validationErrors, validatePort(fields[2]))
+// 	}
 
-	if !(fields[3] == "allow" || fields[3] == "deny") {
-		validationErrors = append(validationErrors, fmt.Errorf("%w. %q must be one of 'allow' or 'deny'", ErrInvalidAction, fields[3]))
-	}
-	return validationErrors
-}
-
-func validatePort(p string) error {
-	// TODO
-	return nil
-}
-
-var (
-	ErrInvalidAction   = errors.New("invalid action")
-	ErrInvalidPort     = errors.New("invalid port or port range")
-	ErrInvalidProtocol = errors.New("invalid protocol")
-)
+// 	if !(fields[3] == "allow" || fields[3] == "deny") {
+// 		validationErrors = append(validationErrors, fmt.Errorf("%w. %q must be one of 'allow' or 'deny'", rules.ErrInvalidAction, fields[3]))
+// 	}
+// 	return validationErrors
+// }
